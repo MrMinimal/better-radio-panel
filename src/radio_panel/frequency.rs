f61dc824bc2df541aa49330cb526f149a77f44f9
Tom Langwaldt <t.langwaldt@gmail.com>
2024-02-23 17:51:38 +0100

    Add formatting tests

--- src/radio_panel/frequency.rs
+++ src/radio_panel/frequency.rs
@@ -26,3 +26,35 @@ pub fn format_frequency(freq: Frequency, fractional_digits: u8) -> String {
         }
     }
 }
+
+mod frequency_formatting_tests {
+    use crate::{format_frequency, Frequency};
+
+    #[test]
+    fn test_two_digits() {
+        let freq1: Frequency = Frequency{integer: 108, fraction: 0};
+        let freq2: Frequency = Frequency{integer: 108, fraction: 50};
+        let freq3: Frequency = Frequency{integer: 108, fraction: 100};
+        let freq4: Frequency = Frequency{integer: 108, fraction: 150};
+        let freq5: Frequency = Frequency{integer: 108, fraction: 200};
+        assert_eq!(format_frequency(freq1, 2), "108.00");
+        assert_eq!(format_frequency(freq2, 2), "108.05");
+        assert_eq!(format_frequency(freq3, 2), "108.10");
+        assert_eq!(format_frequency(freq4, 2), "108.15");
+        assert_eq!(format_frequency(freq5, 2), "108.20");
+    }
+
+    #[test]
+    fn test_three_digits() {
+        let freq1: Frequency = Frequency{integer: 108, fraction: 0};
+        let freq2: Frequency = Frequency{integer: 108, fraction: 50};
+        let freq3: Frequency = Frequency{integer: 108, fraction: 100};
+        let freq4: Frequency = Frequency{integer: 108, fraction: 150};
+        let freq5: Frequency = Frequency{integer: 108, fraction: 200};
+        assert_eq!(format_frequency(freq1, 3), "08.000");
+        assert_eq!(format_frequency(freq2, 3), "08.050");
+        assert_eq!(format_frequency(freq3, 3), "08.100");
+        assert_eq!(format_frequency(freq4, 3), "08.150");
+        assert_eq!(format_frequency(freq5, 3), "08.200");
+    }
+}
